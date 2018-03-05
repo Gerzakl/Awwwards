d68ab6b068a7fb94dc9d3a2ac98412b931a0ff74
bananaWeb <a89.volkov@gmail.com>
2018-03-05 21:13:09 +0300

    chetkie

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,22 +2,45 @@ $(document).ready(function(){
 
 	let random = Math.floor(Math.random() * (5 - 1) + 1);
 
+	let modalToggler = $('.js-modal-toggler'),
+		modal = $('.modal'),
+		headerMobile = $('.header-mobile'),
+		modalElems = [
+			'.js-modal-github',
+			'.js-modal-vk',
+			'.js-modal-mail',
+			'.modal-map',
+			'.modal-toggler'
+		];
+
+
 	$('.section-main').css({
 		'background-image': 'url(./assets/images/bg-' + random + '.jpg)'
 	});
 
 	$('.mobile-toggler-js').click(function() {
-		$('.header-mobile').addClass('-open');
-		$('.header-mobile').addClass('-Xvisible');
+		headerMobile.addClass('-open');
+		headerMobile.addClass('-Xvisible');
 	})
 
 	$('.mobile-togglerX-js').click(function() {
-		$('.header-mobile').removeClass('-open');
-		$('.header-mobile').removeClass('-Xvisible');
+		headerMobile.removeClass('-open');
+		headerMobile.removeClass('-Xvisible');
 	})
 
 	setTimeout(function(){
 		$('h1').addClass('-animated');
 		$('h2').addClass('-animated');
 	}, 1500);
+  
+	modalToggler.click(function() {
+		modal.toggleClass('-active');
+			for (let i = 0; i < modalElems.length; i++) {
+				let timeToWait = i * 200;
+				setTimeout(function() {
+				$(modalElems[i]).toggleClass('-active');
+			}, 200 + timeToWait);
+		}
+	});
+
 });
